5ce74b258ba5e848db0f533d87e57f08d03679f4
Hananideen <hananee92@gmail.com>
2015-08-04 21:58:31 +0800

    Removed address edit text

--- app/src/main/java/com/fyp/melody/CheckoutActivity.java
+++ app/src/main/java/com/fyp/melody/CheckoutActivity.java
@@ -18,10 +18,10 @@ public class CheckoutActivity extends Activity {
     SharedPreferences settings;
     SharedPreferences.Editor editor;
 
-    private EditText editName, editHome, editStreet, editPostcode, editCity, editPass;
+    private EditText editName, editPass;
     private Button buttonConfirm;
     private String password, savedPassword;
-    private RadioButton address1;
+    private RadioButton address1, address2;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -33,21 +33,10 @@ public class CheckoutActivity extends Activity {
         editName = (EditText) findViewById(R.id.ProfileUsername);
         editName.setText(settings.getString("userName", ""));
 
-        editHome = (EditText) findViewById(R.id.editHome);
-        editHome.setText(settings.getString("Home", ""));
-
-        editStreet = (EditText) findViewById(R.id.editStreet);
-        editStreet.setText(settings.getString("Street", ""));
-
-        editPostcode = (EditText) findViewById(R.id.editPostcode);
-        editPostcode.setText(settings.getString("Postcode", ""));
-
-        editCity = (EditText) findViewById(R.id.editCity);
-        editCity.setText(settings.getString("City", ""));
-
         editPass = (EditText) findViewById(R.id.editPass);
 
         address1 = (RadioButton) findViewById(R.id.radio1);
+        address1.setText("Main Address: " + settings.getString("Home", ""));
         address1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,6 +45,8 @@ public class CheckoutActivity extends Activity {
             }
         });
 
+        address2 = (RadioButton) findViewById(R.id.radio2);
+        address2.setText("Delivery Address: " + settings.getString("Home", ""));
 
         savedPassword = settings.getString("Password", "");
 
@@ -67,17 +58,13 @@ public class CheckoutActivity extends Activity {
                 password = editPass.getText().toString();
                 if (password.equals(savedPassword)) {
                     editor.putString("userName", editName.getText().toString());
-                    editor.putString("Home", editHome.getText().toString());
-                    editor.putString("Street", editStreet.getText().toString());
-                    editor.putString("Postcode", editPostcode.getText().toString());
-                    editor.putString("City", editCity.getText().toString());
                     editor.commit();
 
                     Intent track = new Intent(CheckoutActivity.this, TrackingActivity.class);
                     startActivity(track);
 
                 } else if (password.length()==0) {
-                    Toast.makeText(getApplicationContext(), "Please insert your password.", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Please insert your password", Toast.LENGTH_LONG).show();
                 }
                 else {
                     Toast.makeText(getApplicationContext(), "The password is incorrect", Toast.LENGTH_LONG).show();
