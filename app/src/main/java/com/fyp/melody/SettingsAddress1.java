d5d53b52039e384f0274e2521772bceac6373174
Hananideen <hananee92@gmail.com>
2015-08-09 18:16:53 +0800

    Added tap on map to select location

--- app/src/main/java/com/fyp/melody/SettingsAddress1.java
+++ app/src/main/java/com/fyp/melody/SettingsAddress1.java
@@ -12,8 +12,12 @@ import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 /**
  * Created by Hananideen on 4/8/2015.
@@ -23,9 +27,10 @@ public class SettingsAddress1 extends AppCompatActivity {
     SharedPreferences settings;
     SharedPreferences.Editor editor;
     GoogleMap map;
+    ArrayList<LatLng> markerPoints;
     private EditText editHome, editStreet, editSearch;
     private Button buttonConfirm, buttonCancel, buttonSearch;
-    private String home, search;
+    private String home, search, savedLatitude, savedLongitude;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,15 +40,42 @@ public class SettingsAddress1 extends AppCompatActivity {
         editor = settings.edit();
 
         SupportMapFragment fm = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        markerPoints = new ArrayList<LatLng>();
         map = fm.getMap();
         map.setMyLocationEnabled(true);
+
+        savedLatitude = settings.getString("Latitude", "");
+        savedLongitude = settings.getString("Longitude", "");
+
+        Toast.makeText(getApplicationContext(), "" +savedLatitude +"," +savedLongitude , Toast.LENGTH_LONG).show();
+
+//        Double latDouble = Double.parseDouble(savedLatitude);
+//        Double longDouble = Double.parseDouble(savedLongitude);
+
+        LatLng location = new LatLng(2.927, 101.641);
+        map.addMarker(new MarkerOptions().position(location).title("Your location"));
+        CameraUpdate zoomLocation = CameraUpdateFactory.newLatLngZoom(location, 15);
+        map.animateCamera(zoomLocation);
+
         map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng point) {
 
+                if(markerPoints.size()>0){
+                    markerPoints.clear();
+                    map.clear();
+                }
+
+                markerPoints.add(point);
                 MarkerOptions options = new MarkerOptions();
                 options.position(point);
-                map.addMarker(options);
+                map.addMarker(options.title("Your location" + point.latitude + point.longitude));
+
+                String latitude = String.valueOf(point.latitude);
+                String longitude = String.valueOf(point.longitude);
+                editor.putString("Latitude", latitude);
+                editor.putString("Longitude", longitude);
+                editor.commit();
 
             }
         });
@@ -61,7 +93,6 @@ public class SettingsAddress1 extends AppCompatActivity {
                     LatLng mmu = new LatLng(2.927, 101.641);
                     CameraUpdate mmuLocation = CameraUpdateFactory.newLatLngZoom(mmu, 15);
                     map.animateCamera(mmuLocation);
-                    map.addMarker(new MarkerOptions().position(mmu).title("MMU"));
 
                 }
 
