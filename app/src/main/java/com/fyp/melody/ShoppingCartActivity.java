f660de2e6abcdb09dac6b1adf770bb5348e035d9
Hananideen <hananee92@gmail.com>
2015-08-18 01:40:32 +0800

    Fixed subtotal

--- app/src/main/java/com/fyp/melody/ShoppingCartActivity.java
+++ app/src/main/java/com/fyp/melody/ShoppingCartActivity.java
@@ -19,23 +19,19 @@ public class ShoppingCartActivity extends ActionBarActivity implements View.OnCl
 
     private List<Product> mCartList;
     private ProductAdapter mProductAdapter;
-    private DecimalFormat df;
-    double subTotal = 0.00;
+    String total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
 
-
         mCartList = ShoppingCartHelper.getCartList();
-        df = new DecimalFormat("@@##");
 
         // Make sure to clear the selections
         for(int i=0; i<mCartList.size(); i++) {
             mCartList.get(i).selected = false;
         }
-
 
         // Create the list
         final ListView listViewCatalog = (ListView) findViewById(R.id.ListViewCatalog);
@@ -47,7 +43,7 @@ public class ShoppingCartActivity extends ActionBarActivity implements View.OnCl
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position,
                                     long id) {
-                Intent productDetailsIntent = new Intent(getBaseContext(),ProductDetails.class);
+                Intent productDetailsIntent = new Intent(getBaseContext(), ProductDetails.class);
                 productDetailsIntent.putExtra(ShoppingCartHelper.PRODUCT_INDEX, position);
                 startActivity(productDetailsIntent);
             }
@@ -58,24 +54,27 @@ public class ShoppingCartActivity extends ActionBarActivity implements View.OnCl
     @Override
     protected void onResume() {
         super.onResume();
-
         // Refresh the data
-        if(mProductAdapter != null) {
-            mProductAdapter.notifyDataSetChanged();
-        }
+        if(mProductAdapter != null)
+            {
+                mProductAdapter.notifyDataSetChanged();
+            }
+        double subTotal = 0.00;
+            for (Product p : mCartList) {
+                int quantity = ShoppingCartHelper.getProductQuantity(p);
 
-        for(Product p : mCartList) {
-            int quantity = ShoppingCartHelper.getProductQuantity(p);
-            subTotal += p.price * quantity;
-        }
+                subTotal += p.price * quantity;
+            }
 
-        TextView productPriceTextView = (TextView) findViewById(R.id.TextViewSubtotal);
-        productPriceTextView.setText("Subtotal: RM" + String.format("%.2f", subTotal));
+            TextView productPriceTextView = (TextView) findViewById(R.id.TextViewSubtotal);
+            productPriceTextView.setText("Subtotal: RM" + String.format("%.2f", subTotal));
+        total = String.format("%.2f", subTotal);
+
     }
 
     public void checkout (View v) {
         Intent payment = new Intent(getBaseContext(), PaymentActivity.class);
-        payment.putExtra("subtotal", String.format("%.2f", subTotal));
+        payment.putExtra("subtotal", total);
         startActivity(payment);
     }
 
